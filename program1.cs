ead11a4dcb28b61890addb4ddc2a2c160716abd2
Dhiraj kushwaha <163713169+programming-nothing2@users.noreply.github.com>
2024-09-04 16:45:34 +0545

    Add files via upload

---
+++ program1.cs
@@ -0,0 +1,15 @@
+
+// 1. WAP to print text or any message.
+
+using System;
+
+namespace Program
+{
+	public class program1
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Message is printed.");
+		}
+	}
+}
---
+++ program2.cs
@@ -0,0 +1,21 @@
+
+// 2. WAP to demonstrate examples of Console.Write and Console.WriteLine.
+
+using System;
+
+namespace Program
+{
+	public class program2
+	{
+		public static void Main(string[] args)
+		{
+		  
+		  // only print the Message in single line
+		  Console.Write("Hello");
+		  
+		  // print the newline after printing the message.
+		  Console.WriteLine();// for new line
+			Console.WriteLine("Message is printed.");
+		}
+	}
+}
